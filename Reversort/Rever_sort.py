7d35fbff3683d238fa1d887227eac63b3e94e33c
Mehmet5353 <44751934+Mehmet5353@users.noreply.github.com>
2021-05-02 13:29:55 +0300

    Add files via upload

---
+++ Reversort/Rever_sort.py
@@ -0,0 +1,11 @@
+N=int(input())
+for j in range(N):
+    Num=int(input())
+    a=list(map(int,input().split()))
+    cost=0
+    for i in range(len(a)-1):
+        MIN=i+1
+        Index=a.index(MIN)
+        a=a[0:i]+a[i:Index+1][::-1]+a[Index+1:len(a)]
+        cost+=Index-i+1
+    print("Case #"+str(j+1)+": "+str(cost))
